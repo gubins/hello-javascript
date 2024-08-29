0dc24a58bb3e397b4cdae64d97d026574adad4ba
gubins@gmail.com <gubins@gmail.com>
2024-08-29 16:47:37 +0200

    01

---
+++ Basic/01-variables-g.js
@@ -0,0 +1,21 @@
+//var
+var holaMon="Hola mon"
+console.log(holaMon)
+holaMon="Hola SHS"
+console.log(holaMon)
+//let
+let holaMon2="Hola mon"
+console.log(holaMon2)
+
+holaMon2="Hola SHS"
+console.log(holaMon2)
+
+//const
+
+const holaMon3="hola mon constant"
+console.log(holaMon3)
+/*
+dona error
+holaMon3="prova"
+console.log(holaMon3)
+*/
