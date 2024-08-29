7b36cb2b6f4ded5790393b93f070b7c8f1929fa1
gubins@gmail.com <gubins@gmail.com>
2024-08-29 17:43:20 +0200

    02

---
+++ Basic/02-datatypes-g.js
@@ -0,0 +1,49 @@
+//cadena de text
+let myName="Nom Primer Segon"
+let alias="Gubins"
+let email='bustia@correu.cat'
+
+//Numeros
+let age=48 //enter
+let height=1.75 //decimal
+
+//Booleanos
+let isStudent=true
+let isTeacher=false
+
+//undefined
+let undefinedValue
+console.log(undefinedValue)
+
+//null
+let nullValue=null
+console.log(nullValue)
+
+//symbol
+let mySymbol=Symbol("mysymbol")
+
+//bigint
+let myBigInt = BigInt(817239871289371986589716389471628379612983761289376129)
+let myBigInt2 = 817239871289371986589716389471628379612983761289376129n
+console.log(myBigInt2/myBigInt)
+
+
+// Mostramos los tipos de datos
+console.log(typeof myName)
+console.log(typeof alias)
+console.log(typeof email)
+
+console.log(typeof age)
+console.log(typeof height)
+
+console.log(typeof isTeacher)
+console.log(typeof isStudent)
+
+console.log(typeof undefinedValue)
+
+console.log(typeof nullValue)
+
+console.log(typeof mySymbol)
+
+console.log(typeof myBigInt)
+console.log(typeof myBigInt2)
