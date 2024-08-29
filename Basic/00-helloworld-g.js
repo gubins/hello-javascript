e4419e2137f0e82ce7fd1c3532ceb84aca3ca248
gubins@gmail.com <gubins@gmail.com>
2024-08-29 16:31:50 +0200

    00-holamon

---
+++ Basic/00-helloworld-g.js
@@ -0,0 +1,13 @@
+console.log("hola mon")
+console.log('hola mon')
+console.log(`hola mon`)
+
+//comentari
+/*
+varies línies
+*/
+
+console.log("5")
+console.log(5)
+console.log(5*2)
+console.log(3**3)
